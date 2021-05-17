d36d109c30602b14a823ba977c7fcd0e156bd5d8
Chi Tran <khanhchi.tran13051@gmail.com>
2021-05-17 18:53:03 +0200

    update linkedin

--- src/components/Sidebar/sidebar.js
+++ src/components/Sidebar/sidebar.js
@@ -1,8 +1,8 @@
-import React from "react";
-import GithubIcon from "../../assets/icons/github-brands.svg";
-import FacebookIcon from "../../assets/icons/facebook-square-brands.svg";
-import LinkedinIcon from "../../assets/icons/linkedin-brands.svg";
-import "./sidebar.scss";
+import React from "react"
+import GithubIcon from "../../assets/icons/github-brands.svg"
+import FacebookIcon from "../../assets/icons/facebook-square-brands.svg"
+import LinkedinIcon from "../../assets/icons/linkedin-brands.svg"
+import "./sidebar.scss"
 function SideBar() {
   return (
     <ul className="side-bar">
@@ -16,12 +16,12 @@ function SideBar() {
         <FacebookIcon className="icon facebook" />
       </li>
       <li className="side-bar_item">
-        <a href="https://www.linkedin.com/in/chi-tran-6393b7204/">
+        <a href="https://www.linkedin.com/in/chitran-de/">
           <LinkedinIcon className="icon linkedin" />
         </a>
       </li>
     </ul>
-  );
+  )
 }
 
-export default SideBar;
+export default SideBar
