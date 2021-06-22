3663e9b79cfd420a65d9763e28f757d8da58e33f
Chi Tran <khanhchi.tran13051@gmail.com>
2021-06-22 12:32:04 +0200

    fixed cv link

--- src/pages/about.js
+++ src/pages/about.js
@@ -35,7 +35,7 @@ const About = ({ location }) => {
           refining my skills with the right company.
         </p>
         <Button>
-          <a href="../../static/cv-chitran.pdf">Download My CV</a>
+          <a href="/cv-chitran.pdf">Download My CV</a>
         </Button>
         <Button>
           <a href="/projects">My Projects</a>
